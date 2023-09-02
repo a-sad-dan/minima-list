67828e8292e03439984039adc54b3c2bd1caf350
Dan <danishasad91@gmail.com>
2023-09-02 17:11:10 +0530

    Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  watch: true,
+  entry: './src/index.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
+  plugins: [
+
+    new HtmlWebpackPlugin({
+
+      title: 'Minima-List',
+      template: 'src/index.html',
+      favicon: path.resolve(__dirname, 'src', 'MinimaListLogo.svg'),
+    }),
+
+  ],
+
+  module: {
+
+    rules: [
+
+      {
+        test: /\.css$/i,
+
+        use: ['style-loader', 'css-loader'],
+
+      },
+      {
+
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+        type: 'asset/resource',
+
+      },
+    ],
+
+  },
+};
