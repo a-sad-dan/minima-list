9bed2717561b8e4d61f9a0d0b56b8d2482d972a4
Dan <danishasad91@gmail.com>
2024-01-18 23:56:14 +0530

    add sidebar toggle + dummy data

--- src/index.js
+++ src/index.js
@@ -1,13 +1,71 @@
+// imports
 import './style.css'
+import './css/project.css'
+import './css/todo.css'
 
 import projectManager from './js/projectManager'
 import todoManager from './js/toDoManager';
+import { createTodo } from './js/toDoFactory';
+import { createProject } from './js/projectFactory';
+import UIcontroller from './js/UIController';
+
+// Initialise Application
+const homeProject = projectManager.createNewProject('Home');
+
+const p2 = projectManager.createNewProject('Anatomy');
+
+// Set initial project to active
+projectManager.switchProject(homeProject);
 
 
-const defaultProject = projectManager.createNewProject('default');
-console.log(defaultProject);
+// Add dummy tasks to the project
+todoManager.createNewTodo(homeProject, "Buy Groceries", "1 kg tomatoes, 1kg potato", "29-01-2024", "medium");
+todoManager.createNewTodo(homeProject, "Study for exams", "", "29-01-2024", "high");
+todoManager.createNewTodo(homeProject, "Top Realty", "Gift her sth", "23-01-2024", "medium");
 
-todoManager.createNewTodo(defaultProject, 'todo 1', 'do tasks', null, 'high')
+todoManager.createNewTodo(homeProject, "Buy Groceries", "1 kg tomatoes, 1kg potato", "29-01-2024", "medium");
+todoManager.createNewTodo(homeProject, "Study for exams", "", "29-01-2024", "high");
+todoManager.createNewTodo(homeProject, "Top Realty", "Gift her sth", "23-01-2024", "medium");
 
-console.log("projects", projectManager.viewAllProjects(defaultProject))
-console.log('todos in default project', projectManager.viewTodosInProject(defaultProject))
+
+// Render the projects based on the UI
+
+// First render the projects div
+UIcontroller.updateProjectList();
+UIcontroller.updateTodo(homeProject);
+
+
+//todo Include the code after this in its own module
+const sideBar = document.getElementById('sidebar');
+const closeSidebarBtn = document.getElementById('close-sidebar');
+const openSidebarBtn = document.getElementById('open-sidebar');
+
+
+// Close and Open Functionality
+closeSidebarBtn.addEventListener('click', () => toggleSideBar(true));
+openSidebarBtn.addEventListener('click', () => toggleSideBar(false))
+const heading = document.getElementById('heading');
+
+function toggleSideBar(isSideBarOpen) {
+    if (isSideBarOpen) {
+        sideBar.classList.add('closed');
+        heading.classList.add('padded');
+
+    }
+    else {
+        sideBar.classList.remove('closed');
+        heading.classList.remove('padded');
+    }
+}
+
+function resizeSideBar() {
+    if (window.innerWidth < 768) {
+        sideBar.classList.add('closed');
+    } else {
+        sideBar.classList.remove('closed');
+    }
+}
+
+resizeSideBar();
+// for dynamically closing SideBar
+window.addEventListener('resize', resizeSideBar);
