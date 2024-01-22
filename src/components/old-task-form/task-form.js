bc7f6aa089b694d66d7385d341c80d1ab6a5d474
Dan <danishasad91@gmail.com>
2024-01-23 00:41:51 +0530

    add edit form

--- src/components/old-task-form/task-form.js
+++ src/components/old-task-form/task-form.js
@@ -71,7 +71,6 @@ const TaskForm = (() => {
         Modal.closeModal();
     }
 
-
     function saveProject(event) {
         event.preventDefault();
         // Collecting Data from the form
@@ -92,9 +91,93 @@ const TaskForm = (() => {
     }
 
 
-    const newTask = () => taskForm;
 
-    return { newTask }
+    const editTodoForm = (todo) => {
+        const editForm = document.createElement('form');
+        editForm.classList.add('task-form');
+        editForm.innerHTML = `
+                        <img src="${closeBtn}" alt="" class="close-btn">
+                        <div class="form-row">
+                            <label for="taskName" class="hidden"">Task Name</label>
+                            <input type="text" id="taskName" name="taskName" placeholder="Task Name" value = "${todo.title}" required>
+                        </div>
+                        <div class="form-row">
+                            <label class="dueDate">Due Date:
+                                <input type="date" name="date" id="dueDate" value = "${todo.dueDate}">
+                            </label>
+                        </div>
+                        <div class="form-row">
+                            <label class="color">
+                                Color :
+                                <label class="container">
+                                    <input type="radio" name="color" id="high" value="high">
+                                    <span class="red checkmark"></span>
+                                </label>
+                                <label class="container">
+                                    <input type="radio" name="color" id="medium" value="medium">
+                                    <span class="yellow checkmark"></span>
+                                </label>
+                                <label class="container">
+                                    <input type="radio" name="color" id="low"  value="low" checked>
+                                    <span class="green checkmark"></span>
+                                </label>
+                            </label>
+                        </div>
+                        <div class="form-row">
+                            <label for = "description" class = "hidden">Description</label>
+                            <textarea name="description" id ="description" cols placeholder="Add Description..." resize = "true">${todo.description}</textarea>
+                        </div>
+                        <button type="submit" class="submit-btn">
+                            <img src="${submitBtn}" alt="submit">
+                        </button>`
+
+        //todo add logic to add checked attribute to the correct priority
+        editForm.addEventListener('click', function (event) {
+            const btn = event.target;
+            if (btn.matches('.close-btn')) {
+                hideForm();
+            }
+        });
+
+        editForm.addEventListener('submit', (event) => handleUpdate(event));
+
+        function handleUpdate(event) {
+            event.preventDefault();
+
+            let taskData = new FormData(editForm);
+            const newTaskName = taskData.get('taskName');
+            const newDueDate = taskData.get('date');
+            const newPriority = taskData.get('color').toLowerCase();
+            const newDescription = taskData.get('description');
+
+            // change todo values
+            todo.title = newTaskName;
+            todo.dueDate = newDueDate;
+            todo.description = newDescription;
+            todo.priority = newPriority;
+
+            // reset form --> close form
+            event.target.reset();
+            Modal.closeModal();
+
+            // force ui re render
+            UIcontroller.updateProjectList() //?can remove this???
+            UIcontroller.updateTodo(projectManager.getActiveProject());
+
+
+            event.target.reset();
+            Modal.closeModal();
+
+            // force UI re render
+            UIcontroller.updateProjectList()
+            UIcontroller.updateTodo(projectManager.getActiveProject());
+
+        }
+        return editForm;
+    }
+
+    const newTask = () => taskForm;
+    return { newTask, editTodoForm }
 })();
 
 export default TaskForm;
