287691eee49c7551b072f1e51f2f49bb0995f044
Dan <danishasad91@gmail.com>
2024-01-23 00:49:02 +0530

    add edit feature

--- src/js/UIController.js
+++ src/js/UIController.js
@@ -2,7 +2,12 @@
 import projectManager from "./projectManager";
 
 const projectsContainer = document.getElementById('projects-container');
-const contentDiv = document.getElementById('content')
+const contentDiv = document.getElementById('content');
+const subheading = document.getElementById('subheading');
+
+
+import editIcon from './../assets/icons/pencil.svg'
+import expandIcon from './../assets/icons/expand.svg'
 
 
 const UIcontroller = (() => {
@@ -11,6 +16,7 @@ const UIcontroller = (() => {
         projectsContainer.innerHTML = '';
         const allProjects = projectManager.viewAllProjects();
         allProjects.forEach((item) => { renderProject(item) })
+
     }
 
     const updateTodo = (project) => {
@@ -20,14 +26,20 @@ const UIcontroller = (() => {
         const allTodos = projectManager.viewTodosInProject(project)
         allTodos.forEach((item) => { renderTodo(item) });
 
+        subheading.innerText = ` ${project.name}`
+
+        updateProjectList()
     }
 
-    const expandTodoDetails = (todo) => {
+    const toggleExpandDetails = (todo) => {
         // Expand a todo item to display and edit its details
     }
 
-    const showError = () => {
+    const showError = (message) => {
         //Display Error messages to the user
+        const errorMessage = document.createElement('p')
+        errorMessage.classList.add('error-message');
+        Modal.renderModal(errorMessage);
     }
     const clearFields = () => {
         // Clear input field after input/editing todos
@@ -39,29 +51,64 @@ const UIcontroller = (() => {
 
 export default UIcontroller;
 
+import todoManager from "./toDoManager";
+import ProjectForm from "../components/project-form/project-form";
+import Modal from "../components/modal/modal";
+import TaskForm from "../components/old-task-form/task-form";
 
 // Helper Functions
 const renderProject = (project) => {
-    // console.log(project)
-    // change this function to make dom elements and append them to the projects Div
-    const card = projectCardGenerator(project);
-    // Attach event listener to listen for render 
-    card.addEventListener('click', () => {
 
+    const card = projectCardGenerator(project);
+
+    // Attach event listener to listen for render 
+    card.addEventListener('click', (event) => {
+
+        // make clicked project as active and render the tasks in it
         projectManager.switchProject(project);
         UIcontroller.updateTodo(projectManager.getActiveProject())
+
+        // if edit button clicked
+        if (event.target.matches('.edit-btn')) {
+            Modal.renderModal(ProjectForm.newEditForm(project));
+        }
     });
 
     // add card to sidebar
     projectsContainer.append(card);
-
-
 }
+
 
 const renderTodo = (todo) => {
     // console.log(todo);
-    contentDiv.append(todoCardGenerator(todo));
+    const todoCard = todoCardGenerator(todo);
+    todoCard.addEventListener('click', (event) => handleTodoClick(event))
+
+    //handle state of the todo card
+    function handleTodoClick(event) {
+        if (event.target.matches('.edit-btn')) {
+
+            Modal.renderModal(TaskForm.editTodoForm(todo));
+        }
+        if (event.target.matches('.expand-btn')) toggleExpand();
+
+        if (event.target.matches('.toggle-done')) {
+            todoManager.toggleTodoStatus(todo);
+            todoCard.classList.toggle('completed');
+        };
+        // console.table(projectManager.viewAllProjects()[0].todos);
+    }
+    contentDiv.append(todoCard);
+
+    function toggleExpand() {
+        todoCard.querySelector('.description').classList.toggle('hidden');
+        todoCard.querySelector('.expand-btn').classList.toggle('flipped')
+    }
+
 }
+
+
+
 
 const projectCardGenerator = (project) => {
     const card = document.createElement('div');
@@ -71,6 +118,7 @@ const projectCardGenerator = (project) => {
                     <span class = "symbol">${project.name[0]}</span>
                     <p class="name">${project.name}</p>
                     <p class="count">${project.todos.length}</p>
+                    <button class="edit-btn" style = "background : url(${editIcon})"></button>
                 `
     card.innerHTML = cardHtml;
     // console.log(card)
@@ -83,13 +131,20 @@ const todoCardGenerator = (todo) => {
     todoCard.classList.add('todo-card');
     todoCard.dataset.index = todo.index
 
+    if (todo.isDone) todoCard.classList.add('completed');
+
     const cardHtml = `
-                        <div class="priority ${todo.priority}"></div>
+                        <div class="priority ${todo.priority}">
+                            <button class="toggle-done"> </button>
+                        </div>
                         <div class="details">
                             <p class="title">${todo.title}</p>
                             <p class="due-date">${todo.dueDate}</p>
+                            <p class = "description hidden">${todo.description}</p>
                         </div>
-                        <button class="toggleDone">✓</button>
+                        <div class = "actions">
+                            <button class = "edit-btn" style ="background:url(${editIcon}) ;"></button>
+                            <button class = "expand-btn" style ="background:url(${expandIcon})"></button>
                         </div>
                     `
     todoCard.innerHTML = cardHtml;
